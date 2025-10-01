953e78be7053e2b82b43d1be9c7799ec240231e5
Qingyang Lan <qingyanglan@Qingyangs-MacBook-Pro-108.local>
2025-10-01 13:54:33 -0400

    Update

--- PollReader.py
+++ PollReader.py
@@ -54,23 +54,21 @@ class PollReader():
         and each value in a CSV is seperated by a comma.
         """
 
-        # iterate through each row of the data
+       
         for i, row in enumerate(self.raw_data):
-            if i ==0:
+            if i == 0:  
                 continue
-            seperated = row.strip().split(',')
-            # split up the row by column
-            if len(seperated)<5:
+            separated = row.strip().split(',')
+            if len(separated) < 6:  
                 continue
-            
 
-            # map each part of the row to the correct column
-            self.data_dict['month'].append(seperated[0])
-            self.data_dict['date'].append(int(seperated[1]))
-            self.data_dict['sample'].append(int(seperated[2].split(" ")[0]))
-            self.data_dict['sample type'].append(seperated[3])
-            self.data_dict['Harris result'].append(float(seperated[4]))
-            self.data_dict['Trump result'].append(float(seperated[5]))
+            
+            self.data_dict['month'].append(separated[0])
+            self.data_dict['date'].append(int(separated[1]))
+            self.data_dict['sample'].append(int(separated[2].split(" ")[0]))
+            self.data_dict['sample type'].append(separated[3])
+            self.data_dict['Harris result'].append(float(separated[4]))
+            self.data_dict['Trump result'].append(float(separated[5]))
 
 
     def highest_polling_candidate(self):
